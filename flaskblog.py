6f0b0650a3fa61f140657b0e5ee1b0445732779a
douglas254 <dobara@kabarak.ac.ke>
2021-03-13 15:14:02 +0300

    forms and validation

--- flaskblog.py
+++ flaskblog.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 
 
+app.config['SECRET_KEY'] = 'ff1e2033b6671b9129fecaef8e623834'
 posts = [
     {
         'author': 'Douglas Obara',
@@ -29,5 +31,27 @@ def about():
     return render_template('about.html', title='about')
 
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        (f'Account created for { form.username.data }!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'douglas@blog.com' and form.password.data == 'password':
+            flash('you have been logged in succesful', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('unsuccessful')
+    return render_template('login.html', title='Login', form=form)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
+flash
